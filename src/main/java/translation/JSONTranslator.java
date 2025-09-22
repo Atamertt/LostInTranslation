46c89c320deb80ed266121046d769f78417a1049
brad <baoyi.dai@mail.utoronto.ca>
2025-09-22 13:24:52 -0400

    Finish Task C and cancel TODO

--- src/main/java/translation/JSONTranslator.java
+++ src/main/java/translation/JSONTranslator.java
@@ -51,13 +51,11 @@ public class JSONTranslator implements Translator {
 
                 List<String> languages = new ArrayList<>();
 
-                // TODO Task C: record this countryCode in the correct instance variable
                 countryCodes.add(countryCode);
                 // iterate through the other keys to get the information that we need
                 for (String key : countryData.keySet()) {
                     if (!key.equals("id") && !key.equals("alpha2") && !key.equals("alpha3")) {
                         String languageCode = key;
-                        // TODO Task C: record this translation in the appropriate instance variable
                         translations.put(countryCode + "-" + languageCode, countryData.getString(languageCode));
                         if (!languageCodes.contains(languageCode)) {
                             languageCodes.add(languageCode);
@@ -73,7 +71,6 @@ public class JSONTranslator implements Translator {
 
     @Override
     public List<String> getLanguageCodes() {
-        // TODO Task C: return a copy of the language codes
         return new ArrayList<>(languageCodes);
     }
 
@@ -84,7 +81,6 @@ public class JSONTranslator implements Translator {
 
     @Override
     public String translate(String countryCode, String languageCode) {
-        // TODO Task C: complete this method using your instance variables as needed
         if (countryCode == null || languageCode == null) {
             return null;
         }
