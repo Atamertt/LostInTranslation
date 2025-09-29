8d3822d419e3cdceb21a6f7e9bf8494fb55c89ce
Atamert <atamert.cam@mail.utoronto.ca>
2025-09-29 11:14:59 -0400

    Completed Lab, all tasks done

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -38,7 +38,6 @@ public class CountryCodeConverter {
             while (iterator.hasNext()) {
                 String line = iterator.next();
                 String[] parts = line.split("\t");
-                // TODO Task B: use parts to populate the instance variable
                 countryCodeToCountry.put(parts[2].toLowerCase(Locale.ROOT), parts[0]);
                 countryToCountryCode.put(parts[0], parts[2]);
             }
@@ -55,7 +54,6 @@ public class CountryCodeConverter {
      * @return the name of the country corresponding to the code
      */
     public String fromCountryCode(String code) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
         return countryCodeToCountry.get(code);
     }
 
@@ -65,7 +63,6 @@ public class CountryCodeConverter {
      * @return the 3-letter code of the country
      */
     public String fromCountry(String country) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
         return countryToCountryCode.get(country);
     }
 
@@ -74,7 +71,6 @@ public class CountryCodeConverter {
      * @return how many countries are included in this country code converter.
      */
     public int getNumCountries() {
-        // TODO Task B: update this code to use an instance variable to return the correct value
         return countryToCountryCode.size();
     }
 }
